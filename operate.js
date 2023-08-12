7393efc6b0b5dd26c4bf69b6cd7b8146143f3de4
AyongNice <AyongNice@163.com>
2023-08-12 11:29:14 +0800

    网站介绍

--- operate.js
+++ operate.js
@@ -1,16 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     const button = document.getElementById('label');
+    const label2 = document.getElementById('label2');
     button.addEventListener('click', function () {
+        window.open('http://ayongnice.love/chatgpt/')
+    });
+    label2.addEventListener('click', function () {
         window.open('http://ayongnice.love/chatgpt/')
     });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const button = document.getElementsByClassName('follow-btn-inner');
-    button.addEventListener('click', function () {
-        chrome.runtime.sendMessage({sendMessage: '关注微信号阿勇学前端!你将赢来人生巅峰'}, function (response) {
-            alert(response.farewell)
-        });
-
-    });
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     const button = document.getElementsByClassName('follow-btn-inner');
+//     button.addEventListener('click', function () {
+//         chrome.runtime.sendMessage({sendMessage: '关注微信号阿勇学前端!你将赢来人生巅峰'}, function (response) {
+//             alert(response.farewell)
+//         });
+//
+//     });
+// });
